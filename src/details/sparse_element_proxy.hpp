50a589cf139791d4d50a03a6147dee523bd7d3a4
Ilya Biryukov <iu.biryukov@gmail.com>
2011-11-18 12:25:59 +0400

    Moved this header to details folder.

---
+++ src/details/sparse_element_proxy.hpp
@@ -0,0 +1,43 @@
+#ifndef SPARSE_ELEMENT_PROXY_HPP_
+#define SPARSE_ELEMENT_PROXY_HPP_
+
+#include <cassert>
+
+namespace fe { namespace la { namespace details {
+  template<class Scalar>
+  class sparse_element_proxy {
+    public:
+      typedef Scalar scalar_t;
+      // Constructs a proxy to an element not stored in a matrix
+      sparse_element_proxy()
+          : value_ptr_(0) {
+      }
+
+      explicit sparse_element_proxy(Scalar * value_ptr)
+          : value_ptr_(value_ptr) {
+      }
+
+      sparse_element_proxy(sparse_element_proxy const &) = default;
+      sparse_element_proxy(sparse_element_proxy &&) = default;
+      ~sparse_element_proxy() = default;
+
+      // Conversion to scalar
+      operator Scalar() const {
+        return value_ptr_ ? *value_ptr_ : 0;
+      }
+
+      sparse_element_proxy & operator = (scalar_t value) {
+        if (value_ptr_) {
+          *value_ptr_ = value;
+        } else {
+          assert(value == 0);
+        }
+
+        return *this;
+      }
+    private:
+      Scalar * value_ptr_;
+  };
+} } } // namespace fe::la::details
+
+#endif // SPARSE_ELEMENT_PROXY_HPP_
